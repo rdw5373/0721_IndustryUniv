b40687cd272bac1e9b97faba8e3746349ef4340b
rdw5373 <rdw5373@naver.com>
2021-07-22 14:45:30 +0900

    0722_GNB작업완료

--- script/script.js
+++ script/script.js
@@ -1,5 +1,17 @@
 $(function(){
 
+    // GNB 시작
+    $(".mainLi a, #subMenuBG").mouseover(function(){
+        $(".subMenu").stop().slideDown(800);
+        $("div#subMenuBG").stop().slideDown(800);
+    });
+
+    $(".mainLi a, #subMenuBG").mouseout(function(){
+        $(".subMenu").stop().slideUp(300);
+        $("div#subMenuBG").stop().slideUp(300);
+    });
+    // GNB 끝
+
 
 
 });
